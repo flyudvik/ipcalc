3ecbdc69e192878c71b3cffcd274124a54b6b359
amywoodehy <amywoodehy@gmail.com>
2017-02-17 09:56:42 +0600

    Removed deprecated code

--- ipcalc.py
+++ ipcalc.py
@@ -2,9 +2,7 @@ from flask import Flask, json, render_template, request
 from flask import flash
 from werkzeug.contrib.cache import SimpleCache
 
-import exceptions
 import utils
-from exceptions import *
 
 
 app = Flask(__name__)
@@ -33,36 +31,6 @@ def ipcalc():
             'network': network,
             'sizes': sizes
         })
-
-
-@exceptions.deprecated
-@app.route('/deprecated', methods=['GET'])
-def hello_world():
-    network = request.args.get('network')
-    sizes_str = request.args.get('size')
-    if not network or not sizes_str:
-        return render_template("index.html")
-    try:
-        get_hash = hash(network + sizes_str)
-        context = cache.get(get_hash)
-        if not context:
-            context = {}
-            sizes = json.loads(sizes_str)
-            context['network'] = utils.string_2_network(network)
-            context['sizes'] = sizes
-            required_hosts = utils.get_hosts_with_interface(sizes)
-            subnets = utils.get_subnets_plain(
-                utils.string_2_network(network), min=min(required_hosts)
-            )
-            networks, new_subnets = utils.extract_for_hosts(
-                required_hosts, subnets
-            )
-            context['networks'] = networks
-            context['dedicated'] = utils.count_dedicated_ip(networks)
-            cache.set(get_hash, context, timeout=5 * 60)
-        return render_template("result.html", context=context)
-    except IPCalcBaseException as e:
-        return render_template("error.html", exception=e)
 
 
 app.secret_key = 'kjsdhflkjshflskjdfhlksdfhbvlksefvouhoue,flaedkfhdskf'
